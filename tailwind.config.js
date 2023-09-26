a22d653fcff384a829e1b2f6813be8b80475e948
Andrey <as.odinson@gmail.com>
2023-09-26 17:03:11 +0300

    feat: complete build header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,16 @@ module.exports = {
       borderRadius: {
         'rounded-3xl': '64px',
       },
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+      },
       colors: {
         main: {
           'navy-blue': '#110A1D',
           blue: '#478BF9',
+        },
+        additional: {
+          purple: '#9DADF2',
         },
       },
     },
